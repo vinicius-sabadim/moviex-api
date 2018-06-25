abf9f4bb3f98d511d9d01b09926a89637d08c92a
vinicius-sabadim <vinicius.sabadim@gmail.com>
2018-06-25 19:54:33 -0300

    Removed unused require

--- server/middleware/appMiddleware.js
+++ server/middleware/appMiddleware.js
@@ -1,7 +1,5 @@
 const bodyParser = require('body-parser')
 
-const logger = require('../utils/logger')
-
 module.exports = (app) => {
   app.use(bodyParser.urlencoded({ extended: true }))
   app.use(bodyParser.json())
