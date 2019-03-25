bbbea4f7a8b4c7cdf77a2c5237108008ba49eda6
vinicius-sabadim <vinicius.sabadim@gmail.com>
2019-03-25 10:56:26 +0100

    Fixing deprecation warning

--- server/app.js
+++ server/app.js
@@ -1,13 +1,16 @@
 import express from 'express'
 import cors from 'cors'
+import mongoose from 'mongoose'
 
 import config from './config'
+import appMiddleware from './middleware/appMiddleware'
 
 const app = express()
 app.use(cors())
+appMiddleware(app)
 
-require('mongoose').connect(config.db.url)
-require('./middleware/appMiddleware')(app)
+mongoose.connect(config.db.url)
+mongoose.set('useCreateIndex', true)
 
 const api = require('./api')
 const graphql = require('./graphql')
--- server/middleware/appMiddleware.js
+++ server/middleware/appMiddleware.js
@@ -1,6 +1,6 @@
-const bodyParser = require('body-parser')
+import bodyParser from 'body-parser'
 
-module.exports = (app) => {
+export default app => {
   app.use(bodyParser.urlencoded({ extended: true }))
   app.use(bodyParser.json())
 }
