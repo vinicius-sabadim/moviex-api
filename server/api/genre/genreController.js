2854b8a118f6e2221a5f375ecffbc291845cba19
vinicius-sabadim <vinicius.sabadim@gmail.com>
2019-07-30 15:20:15 +0200

    Including endpoint to get genres

--- server/api/genre/genreController.js
+++ server/api/genre/genreController.js
@@ -3,6 +3,6 @@ import _ from 'lodash'
 import Genre from '../../models/genre'
 
 export const get = async (req, res) => {
-  const genres = await Genre.find()
+  const genres = await Genre.find().sort('name')
   res.json(genres)
 }
